5f95807464affbd147368efa66a7ef21d5d8794e
MakenaMwirigi <makenasharon46@gmail.com>
2025-04-23 15:39:44 +0300

    arithmetics code practice

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-
 // Write your code here
+// multiply
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+// Random
+const random = Math.floor(Math.random() * 100) + 1;
+
+//Modulus
+const num3 = 28;
+const num4 = 6;
+const mod = num3 % num4;
+
+//max
+const max = Math.max(1, 3, 10, 15, 20);
